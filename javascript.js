bd43f0d35b8649ea584bd2ec2997c56fbbef5054
msusana <susymariarodri@gmail.com>
2021-02-22 12:15:12 +0100

    creation beatbox

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let keys = document.querySelectorAll('.key');
 let audios = document.querySelectorAll('audio');
+let beat= document.querySelector('#beat')
 
 
 window.addEventListener('keydown', function (e) {
@@ -11,17 +12,49 @@ window.addEventListener('keydown', function (e) {
         keys.forEach(key => {
             if (e.keyCode == key.getAttribute('data-key')) {
                 key.classList.add('playing')
+                key.addEventListener("transitionend" , function (e){
+                    key.classList.remove('playing', 'sound')    
+                    }, 300);
             }
         })
 })
 
-window.addEventListener('keyup', function (e) {
-            keys.forEach(key => {
-                if (e.keyCode == key.getAttribute('data-key')) {
-                    setTimeout(() =>{
-                    key.classList.remove('playing', 'sound')    
-                    }, 300);
-                    
-                }
-            })
-})
+beat.addEventListener('click', beatbox)
+function beatbox(){
+    function simulateKey(keyCode){ 
+        var eventokey = new Event ('keydown',{ 
+        bubbles:true
+        });
+        eventokey.keyCode = keyCode;
+
+        window.dispatchEvent(eventokey);
+    }
+    function playBeat(time, keyCode){
+        return new Promise((resolve, reject)=>{
+        setTimeout(function(){resolve(simulateKey(keyCode))}, time);
+                
+        })
+    }
+ 
+    
+    playBeat(400,70).then(()=>{
+        return playBeat(400,70)
+    })  
+    .then(()=>{
+        return playBeat(300,65)
+    })
+   
+    .then(()=>{
+        return playBeat(300,67)
+    })
+    .then(()=>{
+        return playBeat(300,67)
+    })
+    .then(()=>{
+        return playBeat(300,77)
+    })
+    .then(()=>{
+        return playBeat(300,71)
+    }) 
+
+}
